eacff2d5c28ccbf349368805cc29066329375ed2
JonathanGuillenDev <jonathanguillencontact@gmail.com>
2021-06-20 01:24:31 -0400

    update to files

---
+++ src/components/BookDetails.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card">
+  <h2 class="book-title">{{ book.title }}</h2>
+   <h3 class="book-author">{{ book.author }}</h3>
+    <img v-if="book.isbn" v-bind:src="'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-M.jpg'" />
+    <p>{{book.read? 'I have read this book' : 'I have not read this book'}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+name: "book-details",
+props: {
+    book: Object
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/BookDetails.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <book-details v-bind:book="book"/>
+  </div>
+</template>
+
+<script>
+import BookDetails from "@/components/BookDetails.vue";
+
+export default {
+  components: {
+    BookDetails,
+  },
+  computed: {
+    book() {
+      return this.$store.state.books.find((b) => {
+        return b.isbn == this.$route.params.isbn;
+      });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
